0f21373c675562dba539e56cb6a5945abdfb6bb2
SEO-Ji-Ho <jhseo9601@gmail.com>
2024-08-26 16:26:01 +0900

    URL-encoded 데이터 파싱으로 수정

--- functions/submit.js
+++ functions/submit.js
@@ -1,13 +1,15 @@
 exports.handler = async (event, context) => {
-    const { name, gender, kakaoId } = JSON.parse(event.body);
-  
+    const params = new URLSearchParams(event.body);
+    const name = params.get('name');
+    const gender = params.get('gender');
+    const kakaoId = params.get('kakaoId');
+
     return {
-      statusCode: 200,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "Content-Type"
-    },
-      body: JSON.stringify({ message: 'Data received successfully!' })
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Content-Type"
+        },
+        body: JSON.stringify({ message: 'Data received successfully!' })
     };
-  };
-  
+};
